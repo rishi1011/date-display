e92589660570298f377c0e9160dfae3b40220986
rishikrishna <r.rishikrishna@gmail.com>
2022-01-20 18:17:28 +0530

    Added Tamil language support

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const dateInput = document.getElementById('date-input');
 const showBtn = document.getElementById('show');
 const result = document.getElementById('result');
 
+dateInput.valueAsDate = new Date();
+
 showBtn.addEventListener('click', () => {
     if (!dateInput.value) {
-        result.textContent = 'Invalid Date';
+        result.textContent = 'தேதியைச் சரிபார்க்கவும்';
         return;
     }
 
     let date = new Date(dateInput.value);
     date.setDate(date.getDate() + 273);
-    result.textContent = date.toDateString();
+    result.textContent = date.toDateString() + ' முதல் நீங்கள் பூஸ்டர் தடுப்பூசி போட்டு கொள்ளலாம்';
 });
+
